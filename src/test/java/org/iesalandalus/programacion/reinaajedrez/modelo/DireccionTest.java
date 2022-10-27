05a0a369bc81acfb49388682b20ecf706f939ee7
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2022-10-27 17:29:46 +0200

    Commit inicial

---
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/DireccionTest.java
@@ -0,0 +1,24 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+class DireccionTest {
+	
+	private static final String NOMBRE_NO_VALIDO = "El nombre de la dirección no es válido.";
+
+	@Test
+	void nombresValidosDirecciones() {
+		assertEquals("Norte", Direccion.NORTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Noreste", Direccion.NORESTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Este", Direccion.ESTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Sureste", Direccion.SURESTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Sur", Direccion.SUR.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Suroeste", Direccion.SUROESTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Oeste", Direccion.OESTE.toString(), NOMBRE_NO_VALIDO);
+		assertEquals("Noroeste", Direccion.NOROESTE.toString(), NOMBRE_NO_VALIDO);
+	}
+
+}
