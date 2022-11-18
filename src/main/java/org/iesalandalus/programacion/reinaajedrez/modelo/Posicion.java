e97ab64693992bb75b4faaa4623a59d37e02d93f
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-18 03:29:39 +0100

    Cuarto Commit
    Creación Clase Posicion
    Atributos fila y columna
    métodos set y get

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -0,0 +1,41 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Posicion {
+	
+	private int fila;
+	private char columna;
+	
+	
+	
+	private void setFila(int fila) 
+	{
+		if (fila<1 && fila>8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+		
+	}
+	
+	public int getFila() 
+	{
+		return fila;
+	}
+	
+	private void setColumna(char columna) 
+	{
+		if (columna<'a' && columna>'h')
+		{
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		this.columna = columna;
+	}
+
+	public char getColumna() 
+	{
+		return columna;
+	}
+	
+
+	
+	
+}
