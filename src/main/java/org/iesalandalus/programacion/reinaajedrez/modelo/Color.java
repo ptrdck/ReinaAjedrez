14ce6fc8c05f01e236731117d46cedc1e51800fe
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-18 03:14:22 +0100

    Tercer Commit
    Creación ENUM Color

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public enum Color {
+	
+	BLANCO("Blanco"),
+	NEGRO("Negro");
+	
+	private String cadenaAMostrar;
+	
+	private Color(String cadenaAMostrar)
+	{
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return cadenaAMostrar;
+	}
+
+}
