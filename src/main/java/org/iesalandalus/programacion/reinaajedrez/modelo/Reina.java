38cbef83ee2369e1d1b4551a693abad3aaa33d41
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-18 04:18:28 +0100

    Decimo Commit
    Creación de métodos Set y Get para Color y Posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -6,5 +6,32 @@ public class Reina {
 	private Posicion posicion;
 	
 	
+	
 
+	public Color getColor() 
+	{
+		return color;
+	}
+	
+	private void setColor(Color color) 
+	{
+		if (color == null)
+		{
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+		this.color = color;
+	}
+	
+	public Posicion getPosicion() 
+	{
+		return posicion;
+	}
+	
+	private void setPosicion(Posicion posicion) 
+	{
+	
+		this.posicion = posicion;
+	}
 }
+	
+
