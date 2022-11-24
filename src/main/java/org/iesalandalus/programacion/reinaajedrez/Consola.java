f90fb447afc5981936d6e018079fbf843be53633
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-24 02:31:27 +0100

    Décimo Séptimo Commit
    Creación método mostrarMenú

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,9 +1,22 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Posicion;
+
 public class Consola {
 	
 	private Consola()
 	{
+		
+	}
+	
+	public static void mostrarMenu() {
+		
+		System.out.println("Menú REINA DE AJEDREZ");
+		System.out.println("::::::::::::::::::::::");
+		System.out.println("1.- Crear REINA por defecto");
+		System.out.println("2.- Crear REINA eligiendo el color");
+		System.out.println("3.- Mover REINA");
+		System.out.println("4.- Salir");
 		
 	}
 
