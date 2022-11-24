38a8c16b1bec90b0f34b2a00db59f1ef1972f37b
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-24 03:47:01 +0100

    Vigésimo cuarto Commit
    Creación de atributo de clase reina en MainApp

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,6 +1,10 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
+	
+	private static Reina reina;
 	
 	public static void main(String[] args) {
 		
