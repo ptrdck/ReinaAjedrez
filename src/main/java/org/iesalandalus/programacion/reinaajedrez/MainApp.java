87ff25269d761488cbab61fc1039ac97db2e8f8b
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2022-11-24 04:14:52 +0100

    Vigésimo quinto Commit
    Creación métodos crearReinaDefecto y crearReinaColor
    En comentarios Método ejecutarOpción ya que se crearán el resto de
    métodos para luego ser llamados por el Switch.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -8,5 +8,43 @@ public class MainApp {
 	
 	public static void main(String[] args) {
 		
+		
 	}
+	
+	/*
+	private static void ejecutarOpcion(int opcion)
+	{
+		switch (opcion)
+		{
+		case 1:
+		{
+			crearReinaDefecto();
+		}
+		case 2:
+		{
+			reina = new Reina(Consola.elegirOpcion());
+		}
+		case 3:
+		{
+			
+		}
+		case 4:
+		{
+			
+		}
+		}
+	}
+	*/
+	
+	private static void crearReinaDefecto()
+	{
+		reina = new Reina();
+	}
+	
+	private static void crearReinaColor()
+	{
+		reina = new Reina();
+	}
+	
+	
 }
